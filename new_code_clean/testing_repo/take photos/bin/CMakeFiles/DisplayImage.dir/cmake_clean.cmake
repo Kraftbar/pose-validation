98b13c183f3ce42da983b3cce1e2f18362f385a6
Kraftbar <gautenybo@gmail.com>
2022-01-30 19:38:43 +0100

    test

---
+++ new_code_clean/testing_repo/take photos/bin/CMakeFiles/DisplayImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../DisplayImage"
+  "../DisplayImage.pdb"
+  "CMakeFiles/DisplayImage.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DisplayImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
